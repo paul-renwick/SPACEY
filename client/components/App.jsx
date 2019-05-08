2e46b71ce09c02993e99d3422221f498aa57b0e9
Rajesh Sharma <raj.sharma21@outlook.com>
2019-05-09 11:22:10 +1200

    working on login

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './Login'
 
 const App = () => {
   return (
-    <Login />
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Login} />
+      </Switch>
+    </Router>
   )
 }
 
