c206ad64dac1683990cbfb16a063480c67cf318d
Paul Renwick <prenwick@gmail.com>
2019-05-09 10:55:29 +1200

    Installed further dependencies, created .env file, added it to gitignore.

--- server/server.js
+++ server/server.js
@@ -6,4 +6,8 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
+const authRoutes = require('./routes/auth')
+
+server.use('/api/v1/auth', authRoutes)
+
 module.exports = server
