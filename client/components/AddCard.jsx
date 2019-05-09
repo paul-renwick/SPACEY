08200405108a27af5e6ecdb0122fc8fbe4fdbd55
ihakapink <ihakapink@gmail.com>
2019-05-10 10:29:54 +1200

    updated fixes for addcard component

--- client/components/AddCard.jsx
+++ client/components/AddCard.jsx
@@ -1,46 +1,83 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { requestCards, receiveCards } from '../actions/cards'
 
-const AddCard = (props) => {
-  return (
-    <div>
-      <form>
-        <label>
-      New Card:
-          <input type="text" name="" />
-        </label>
-        <label>
-          Add A Question:
-          <input type="text" name="" />
-        </label>
-        <label>
-          Add An Answer:
-          <input type="text" name="" />
-        </label>
-        <button>
-          Submit
-        </button>
-      </form>
-    </div>
-  )
-}
+class AddCard extends React.Component {
+  state = {
+    question: '',
+    answer: ''
+  }
 
-const mapStateToProps = (state) => {
-  return {
-    cards: state.cards
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleClick = e => {
+    e.preventDefault
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Add Card</h1>
+        <input name='question'
+          placeholder='question'
+          value={this.state.question}
+          onChange={this.handleChange}
+        />
+        <br /> <br />
+        <input name='answer'
+          placeholder='answer'
+          value={this.state.answer}
+          onChange={this.handleChange}
+        />
+        <br /> <br />
+
+        <button type='button' onClick={() => this.handleClick()}>Submit</button>
+
+      </React.Fragment>
+    )
   }
 }
+export default AddCard
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    actions: () => {
-      dispatch(requestCards())
-    }
-  }
-}
+//     <div>
+//       <form>
+//         <label>
+//       New Card:
+//           <input type="text" name="" />
+//         </label>
+//         <label>
+//           Add A Question:
+//           <input type="text" name="" />
+//         </label>
+//         <label>
+//           Add An Answer:
+//           <input type="text" name="" />
+//         </label>
+//         <button>
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//
+//
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AddCard)
+// const mapStateToProps = state =>
+//   return
+//     cards: state.cards
+//
+//
+
+// const mapDispatchToProps = dispatch =>
+//   return
+//     actions:  =>
+//       dispatchrequestCards
+//
+//
+//
+
+// export default connect
+//   mapStateToProps,
+//   mapDispatchToProps
+// AddCard
