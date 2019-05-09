354a9cc394bb7e2e9e12d13a8534fd51f9975309
Paul Renwick <prenwick@gmail.com>
2019-05-09 12:46:22 +1200

    Removed bootstrap button

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
 
 export default class Dashboard extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <Link to ='/' style={{ color: 'white' }}><Button variant="info" >Logout</Button></Link>
+        <Link to ='/' ><button variant="info" >Logout</button></Link>
       </React.Fragment>
     )
   }
