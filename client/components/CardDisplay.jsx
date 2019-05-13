a3ef88e7b5192f2fcf2f78350ddc8a4a167888aa
Paul Renwick <prenwick@gmail.com>
2019-05-14 11:59:35 +1200

    Styling card display

--- client/components/CardDisplay.jsx
+++ client/components/CardDisplay.jsx
@@ -2,11 +2,39 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { getCards, updateCard } from '../api/cards'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import blue from '@material-ui/core/colors/blue'
+import purple from '@material-ui/core/colors/purple'
+
+const styles = {
+  palette: {
+    primary: blue,
+    secondary: purple
+  },
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
 
 
 class CardList extends React.Component {
+
   componentDidMount () {
     this.props.dispatch(getCards())
   }
@@ -27,27 +55,30 @@ class CardList extends React.Component {
     } 
   }
 
+ 
+
   render () {
     return (
       <div className='container is-fluid has-text-centered'>
         <div className='carddisplay'>
-          <h1>Card Display:</h1>
+          <Typography align='center' variant='h1'>Card Display:</Typography>
+          
           {this.props.cards.map(card => {
             if (card.id == this.props.match.params.id) {
               return (
-                <article className='message is-info' key={card.id}>
-                  <div className='message-header'>
-                    <p>{card.question}</p>
-                  </div>
-                  <div className='message-body'>
-                  {card.answer}
-                  </div>
-                </article>
+                <Card key={card.id} align='center' className='paper' elevation={5}>
+                <Typography color='primary' variant="h1" component="h1">
+                  {card.question}
+                </Typography>
+              </Card>
               )
             }
           })}
-          <Button type='button' onClick={() => this.handleSubmit()}>PLACE A TICK HERE</Button>
+          <Button type='button' onClick={() => this.handleSubmit()}>Tick</Button>
         <Button type='button'>Return to Card list</Button>
+        <Button variant="contained" color='primary' className={'test'}>
+        Primary
+      </Button>
         </div>
       </div>
     )
@@ -60,5 +91,14 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps)(CardList)
 
+export default connect(mapStateToProps)(withStyles(styles)(CardList))
+
+{/* <article className='message is-info' key={card.id}>
+<div className='message-header'>
+  <p>{card.question}</p>
+</div>
+<div className='message-body'>
+{card.answer}
+</div>
+</article> */}
