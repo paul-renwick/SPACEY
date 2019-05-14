13d2b3297376940707525bfcf80fa143a581f2a5
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-05-14 20:13:49 +1200

    got usuername to display in header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
-
-
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class Header extends React.Component {
-render () {
+  displayPageName () {
+  if (this.props.location.pathname === '/register' || this.props.location.pathname === '/login') {
+    return 
+  } else {
+   return <h3>{this.props.userDetails.username}</h3>
+  }
+}
+  
+  render () {
 
   return (
 <React.Fragment>
   
-<section class="hero is-info">
+<section className="hero is-info">
   <div className="hero-body">
     <img src='images/LittleWhiteLogo.png' style={{width: '75px'}} />
+  </div>
+  <div className="display-Username">
+  <h3>{this.displayPageName()}</h3>
   </div>
 </section>
 
@@ -19,4 +30,10 @@ render () {
 }
 }
 
-export default Header
+function mapStateToProps (state) {
+  return {
+    userDetails: state.userDetails
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Header))
