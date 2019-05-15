f208b8ea2a8be7889dba60cd42d057698b7912b6
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-05-15 14:28:07 +1200

    Working logout and username

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
   displayPageName () {
-  if (this.props.location.pathname === '/register' || this.props.location.pathname === '/login') {
-    return 
+  if (this.props.location.pathname === '/register' || this.props.location.pathname === '/') {
+    return
   } else {
-   return <h3>{this.props.userDetails.username}</h3>
+   return <React.Fragment>
+    <h3>{this.props.userDetails.username}</h3><br />
+   <Link to='/'><h3>Log out</h3></Link>
+   </React.Fragment>
     }
 }
 
 render () {
   return (
 <React.Fragment>
-  
 <section className="hero is-info">
   <div className="hero-body">
     <img id='headerIcon' src='images/LittleWhiteLogo.png' style={{width: '75px'}} />
