ff16330294f774b8942c925137ede83a9c6b8b5c
Paul Renwick <prenwick@gmail.com>
2019-05-15 15:52:23 +1200

    Steve now accessible with password '1'

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -4,7 +4,7 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { id: 1, userName: 'Steve', hash: '#' },
+        { id: 1, userName: 'Steve', hash: '$argon2id$v=19$m=65536,t=2,p=1$EVgHaIfx2bvz7ahqakQaog$4GmsQXltPoOqytMyCyKW3jPhjQlD2HD7fgM4BD9vQVE' },
         { id: 2, userName: 'Debra', hash: '#' },
         { id: 3, userName: 'Claire', hash: '#' }
       ])
