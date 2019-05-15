b6848a7b851813a1fd64aa3fa3f22344e5cacbd2
Kuan Thai Lim <philip.limkt@gmail.com>
2019-05-15 14:36:16 +1200

    completed the login page styling

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -36,7 +36,7 @@ class Login extends React.Component {
       <div className='container is-fluid has-text-centered' >
       <h1 className='title is-1'>SPACEY</h1>
          <img src='images/bigBlueLogo.png' style={{width: '250px'}} />
-          <h3 className='frontTitle is-4 has-text-black'>Login</h3>
+          <h2 className='title is-2 has-text-black'>Login</h2>
           <form>
             <div className='Login'> 
                 <input style={{ textAlign:'center', borderColor:'lightblue' }} 
@@ -59,25 +59,36 @@ class Login extends React.Component {
             </div>
           </form>
         </div>
-        <Segment style={ {
-    textAlign: 'start',
-    padding: '3em 0em',
-    borderWidth: '0px',
-    borderRadius: '0px'
-  } }>
-    <Container id="blurb" text>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-        What is <em>SPACEY?</em>
-      </Header>
-      <p>Welcome to SPACEY the world leader in spaced repition learning! What is spaced repition learning, you may ask? The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it.</p>
-      <p>With properly spaced repetition, you increase the intervals of time between learning attempts. Each learning attempt reinforces the neural connections. For example, we learn a list better if we repeatedly study it over a period of time than if we tackle it in one single burst. We’re actually more efficient this way. Spaced sessions allow us to invest less total time to memorize than one single session, whereas we might get bored while going over the same material again and again in a single session. Of course, when we’re bored we pay less and less attention.</p>
-      <p>
-      The difficulty of spaced repetition is not effort but that it requires forward planning and a small investment of time to set up a system. But in the long run, it saves us time as we retain information and spend less total time learning.
-      </p>
-      <p><Link to='/events/new'>Register for an account now</Link></p>
-      <p>Scientific background: <a href='https://fs.blog/2018/12/spacing-effect/' target='_blank' rel="noopener noreferrer">the spacing effect</a></p>
-    </Container>
-  </Segment>
+        <br /><br /><br /><br />
+        <section className='hero'>
+        
+      {/* <section className='section' style={ {
+        // textAlign: 'start',
+        // padding: '3em 0em',
+        // borderWidth: '0px',
+        // borderRadius: '0px'
+      } }> */}
+          {/* <div className='hero-body has-background-grey-lighter' id="blurb" > */}
+          <div className='hero-body has-background-grey-lighter' id="blurb">
+          <br />
+            <div className='container'>
+                {/* <Header as='h3' style={{ fontSize: '2em' }}>
+                  What is <em>SPACEY?</em>
+                </Header> */}
+                <h1 className='title'>What is <em>SPACEY?</em></h1>
+                <h2 className='subtitle'>
+                  <p>Welcome to SPACEY the world leader in spaced repition learning! What is spaced repition learning, you may ask? The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it.</p>
+                  <p>With properly spaced repetition, you increase the intervals of time between learning attempts. Each learning attempt reinforces the neural connections. For example, we learn a list better if we repeatedly study it over a period of time than if we tackle it in one single burst. We’re actually more efficient this way. Spaced sessions allow us to invest less total time to memorize than one single session, whereas we might get bored while going over the same material again and again in a single session. Of course, when we’re bored we pay less and less attention.</p>
+                  <p>
+                  The difficulty of spaced repetition is not effort but that it requires forward planning and a small investment of time to set up a system. But in the long run, it saves us time as we retain information and spend less total time learning.
+                  </p>
+                  <p><Link to='/events/new'>Register for an account now</Link></p>
+                  <p>Scientific background: <a href='https://fs.blog/2018/12/spacing-effect/' target='_blank' rel="noopener noreferrer">the spacing effect</a></p>
+                </h2>
+            </div>
+            
+          </div>
+        </section>
       </React.Fragment>
     )
   }
